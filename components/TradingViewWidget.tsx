8f84aa76ce546a8fa173b1bdbe7330972cbae00f
99x-dgCandidate-Saranga-1737099400352-at-675482622082 <sarangasama@gamail.com>
2025-12-27 02:26:20 +0530

    Implemented the stocks details

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -1,31 +1,28 @@
-'use client'
-import useTradingViewWidget from '@/app/hooks/useTradingViewWidget';
-import { cn } from '@/lib/utils';
-// TradingViewWidget.jsx
-import React, { useRef, memo } from 'react';
+'use client';
 
-interface TradingviewWidgetProps{
+import React, { memo } from 'react';
+import useTradingViewWidget from "@/app/hooks/useTradingViewWidget";
+import {cn} from "@/lib/utils";
+
+interface TradingViewWidgetProps {
     title?: string;
     scriptUrl: string;
     config: Record<string, unknown>;
-    height?: number,
-    className: string
+    height?: number;
+    className?: string;
 }
 
-const TradingViewWidget = ({title, scriptUrl, config,height = 600, className } : TradingviewWidgetProps ) => {
-  const containerRef = useTradingViewWidget(scriptUrl, config,height);
+const TradingViewWidget = ({ title, scriptUrl, config, height = 600, className }: TradingViewWidgetProps) => {
+    const containerRef = useTradingViewWidget(scriptUrl, config, height);
 
-
-  return (
-    <div className='w-full'>
-      {title && <h3 className='font-semibold text-2xl text-gray-100 mb-5'>{title}</h3>}
-      <div className={cn("tradingview-widget-container", className)} ref={containerRef} >
-        <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }}/>
-        <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets</span></a><span className="trademark"> on TradingView</span></div>
-      </div>
-    </div>
-    
-  );
+    return (
+        <div className="w-full">
+            {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
+            <div className={cn('tradingview-widget-container', className)} ref={containerRef}>
+                <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }} />
+            </div>
+        </div>
+    );
 }
 
 export default memo(TradingViewWidget);
