bb0e701b2768ed62a4c7c22040dd24f502795b12
99x-dgCandidate-Saranga-1737099400352-at-675482622082 <sarangasama@gamail.com>
2025-12-26 18:34:09 +0530

    Implement Auth UI & Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import TradingViewWidget from '@/components/TradingViewWidget'
 import { Button } from '@/components/ui/button'
-import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from '@/lib/constants'
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from '@/lib/constants'
 import React from 'react'
 
 const page = () => {
@@ -14,7 +14,7 @@ const page = () => {
           <TradingViewWidget 
             title='Market Overview'
             scriptUrl={`${scriptUrl}market-overview.js`}
-            config={MARKET_DATA_WIDGET_CONFIG}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
             height={600}
             className="custom-chart"
            />
