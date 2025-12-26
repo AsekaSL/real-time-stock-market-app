613eca1bd856d25ad31fbd3833bb1bf89503f9dc
99x-dgCandidate-Saranga-1737099400352-at-675482622082 <sarangasama@gamail.com>
2025-12-27 01:44:24 +0530

    Impement Daily news summary

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { inngest } from '@/lib/inngest/client'
-import { sendSignUpEmail } from '@/lib/inngest/functons'
+import { sendDailyNewsSummary, sendSignUpEmail } from '@/lib/inngest/functons'
 import {serve} from 'inngest/next'
 
 export const {GET, POST, PUT} = serve({
     client: inngest,
-    functions: [sendSignUpEmail]
+    functions: [sendSignUpEmail, sendDailyNewsSummary]
 })
